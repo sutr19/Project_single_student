93c9a65ba37a0e08f94d90d51ba4758213073bb5
Hailay <hellew.family@gmail.com>
2022-08-05 00:00:44 +0200

    operation ssh_config editing

--- operation.py
+++ operation.py
@@ -25,19 +25,20 @@ while True:
         to_add = required_node-exist_node
         while to_add > 0:
             noden = node_name+str(i)
-            hostn=noden+"\n"
+            hostn=noden
             cmd = "openstack server create --image 'Ubuntu 20.04 Focal Fossa 20210616' --flavor {} --key-name {} --network {}  --security-group {} {}".format(
                 fl, key, net, secgroup, hostn)
             with open("hosts", 'r+') as b:
                 ll=b.readlines()
                 ll.insert(6,noden)
+                ll.insert(7,"\n")
                 b.seek(0)
                 b.writelines(ll)
             os.system(
                 'openstack server list |grep "p-tag-node3" | cut -d"|" -f"5" | cut -d"=" -f"2">temp_ip')
             with open("temp_ip") as f:
                 ip = str(f.read())
-            host = "Hostname " + ip + "\n"
+            host = "Hostname " + ip 
             Host = "      HOST " + noden + "\n"
             with open('ssh_conf', 'w') as f:
                 f.write(Host)
@@ -45,6 +46,7 @@ while True:
             with open('ssh_conf', 'a') as f:
                 f.write("\n      ")
                 f.write(host)
+                f.write("\n")
             os.system('cat ssh_conf | sudo tee -a /etc/ssh/ssh_config >> /dev/null')
             os.system(cmd)
             to_add -= 1
@@ -56,7 +58,19 @@ while True:
         while to_del > 0:
             noden = node_name+str(exist_node)
             cmd = "openstack server delete {}".format(noden)
+            rmnd = "grep -v {} hosts > temphost && mv temphost hosts".format(noden)
             os.system(cmd)
+            os.system(rmnd)
+            with open("/etc/ssh/ssh_config", 'r') as b:
+                for num, line in enumerate(b,1):
+                    if noden in line:
+                        n=num+4
+                        cmd="sudo sed '{},{}d' /etc/ssh/ssh_config>ssh_config".format(num,n)
+                        cr=("sudo rm /etc/ssh/ssh_config")
+                        mv=("sudo mv ssh_config /etc/ssh/")
+                        os.system(cmd)
+                        os.system(cr)
+                        os.system(mv)
             to_del -= 1
             exist_node -= 1
     time.sleep(60/2)
--- test.py
+++ test.py
@@ -4,18 +4,24 @@ import os
 #with open('ssh_conf', 'w') as f:
 #    f.write(Host)
 #os.system('cat ssh_temp>>ssh_conf')
-with open("temp_ip") as f:
-    ip = str(f.read())
-host = "Hostname " + ip + "\n" 
+#with open("temp_ip") as f:
+ #   ip = str(f.read())
+#host = "Hostname " + ip + "\n" 
 #with open('ssh_conf', 'a') as f:
  #   f.write("\n      ")
  #   f.write(host)
 #os.system('cat ssh_conf | sudo tee -a /etc/ssh/ssh_config >> /dev/null')
 
-with open("hosts", 'r+') as b:
-    ll=b.readlines()
-    ll.insert(6,host)
-    b.seek(0)
-    b.writelines(ll)
+with open("/etc/ssh/ssh_config", 'r') as b:
+    for num, line in enumerate(b,1):
+        if "p-tag-node4" in line:
+            n=num+4
+            cmd="sudo sed '{},{}d' /etc/ssh/ssh_config>ssh_config".format(num,n)
+            cr=("sudo rm /etc/ssh/ssh_config")
+            mv=("sudo mv ssh_config /etc/ssh/")
+            os.system(cmd)
+            os.system(cr)
+            os.system(mv)
+
 
         
