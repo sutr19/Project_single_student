9dc8b156b6ac39102e04a677a3db0294189ed3ce
Hailay <hellew.family@gmail.com>
2023-06-17 20:05:36 +0200

    openrc

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,9 @@
 opernrc="$1"
 tag="$2"
 key="$3"
+unset OS_AUTH_URL
+unset OS_USERNAME
+unset OS_PASSWORD
 source $opernrc
 chmod +x ./all/cleanup.py
 ./all/cleanup.py $opernrc $tag $key
--- operate.sh
+++ operate.sh
@@ -2,6 +2,9 @@
 opernrc="$1"
 tag="$2"
 key="$3"
+unset OS_AUTH_URL
+unset OS_USERNAME
+unset OS_PASSWORD
 source $opernrc
 echo "Working on updates..."
 chmod +x ./all/opp.py
