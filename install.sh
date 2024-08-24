98ddda6854f5fe286728c019517ebdaebe7deb6a
sutr19 <triparagirisuryatejaswi@gmail.com>
2024-08-24 08:27:30 +0530

    updated and changed ssh key

--- install.sh
+++ install.sh
@@ -4,18 +4,13 @@ openrc="$1"
 tag="$2"
 key="$3"
 
-# Get the directory where the private key resides
 private_key_dir="$(dirname "$key")"
-
-# Define the paths for the private and public keys
 private_key="$key"
 public_key="$private_key.pub"
 
-# Check if the private key exists
+# check existing ssh_key
 if [ -f "$private_key" ]; then
-  # Check if the public key already exists
   if [ ! -f "$public_key" ]; then
-    # Generate the public key from the private key
     ssh-keygen -y -f "$private_key" > "$public_key"
     if [ $? -ne 0 ]; then
       echo "Error generating SSH public key."
